d568f007305083dc0c3361f0ea7ad7c6e93fe26f
Skula <mikko.k.pitkanen02@gmail.com>
2025-03-09 22:17:03 +0200

    color changed from green to blue

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,29 @@
 export const theme = {
     colors: {
-        green1: '#C1D0C4',
-        green2: '#88A08E',
-        green3: '#64AB74',
-        green4: '#196229',
-        green5: '#0A3413',
+
+        //Nää kannattais varmaan uudelleen nimetä
+        //green1 on valkoinen. Figman proton väripallois tullu varmaan pientä väärinkäsitystä
+        green1: '#E0E6FF',
+        green2: '#E0E6FF',
+        green3: '#578FCA',
+        green4: '#3674B5',
+        green5: '#265281',
         white: '#F9F9F9',
         black: '#0A0A0A',
-        grey: '#6C6C6C'
+        grey: '#888EA0'
+
+        /*
+        Alkuperäiset vihreät värit
+        //#C1D0C4 oli tarkoitettu käyettäväksi ainoastaan tyhjän hakupalkin placeholder tekstille.
+        green1: #C1D0C4, -> #E0E6FF
+        green2: #88A08E, -> #E0E6FF 
+        green3: #64AB74, -> #578FCA
+        green4: #196229, -> #3674B5
+        green5: #0A3413, -> #265281
+        white: #F9F9F9, -> #F9F9F9
+        black: #0A0A0A, -> #0A0A0A
+        grey: #6C6C6C, -> #888EA0
+        */
 
     }
 }
