73fc9a6085e083be3b354c699deab27585ed95f3
AHir <alihirv@uef.fi>
2025-03-12 12:50:55 +0200

    muutoksia tehty

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -47,7 +47,7 @@ const Homepage = () => {
                     <div className="map-container">
                         {/* kartalle paikka */}
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094127!2d144.96305791531692!3d-37.81627977975144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ce6e0!2sMelbourne%20VIC!5e0!3m2!1sen!2sau!4v1611817622553!5m2!1sen!2sau"
+                            src="https://www.google.com/maps/
                             width="400"
                             height="300"
                             style={{ border: 0 }}
