f439c2c92cd177afa7d6a120ad370be4d5a5e569
AHir <alihirv@uef.fi>
2025-03-14 05:09:18 +0200

    korjattu homepagea

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,73 +1,44 @@
-import { useSelector } from "react-redux"
-import { AppState } from "../../store"
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import LanguageSelector from './LanguageSelector';
-import SignIn from './SignIn';
-import TopBar from './TopBar';
-import '../styles/TopBar.css';
-///import data from textContentFI.json
-//import data from textContentENG.json
 
-const MenuBar = ({ data, onLoginClick }) => {
-    return (
-        <nav className="menu-bar">
-            <button>{data.frontPage}</button>
-            <button>{data.advancedSearch}</button>
-            <button>{data.commonQuestions}</button>
-            <button>{data.aboutUs}</button>
-        </nav>
-    );
-};
+
+import "./App.css";
+import "../styles/Homepage.css";
+import TopBar from "../TopBar"; 
 
 const Homepage = () => {
+  return (
+    <div className="homepage">
+      <header>
+        <TopBar />
+      </header>
+      <main>
+        <section id="etusivu">
+          <h1>Etusivu</h1>
+          <p>Yli 1000 kalalupaa helposti saatavilla</p>
+        </section>
 
-    return (
-        <div className="homepage">
-             <header>
-                <TopBar />
-            </header>
-            {/*<header>
-                <div className="logo">Kala-Appi</div>
-                <SearchBar />
-                <LanguageSelector />
-                <button className="login-button" onClick={() => setShowLogin(true)}>{data.loginButton.titile0}</button>
-            </header>*/}
-            <MenuBar data={data.MenuBar} />
-            <main>
-                <section className="intro">
-                    <div className="intro-text">
-                        <h1>{data.frontPage.text0Header}</h1>
-                        <p>{data.frontPage.text0Content}</p>
-                    </div>
-                    <div className="map-container">
-                        {/* kartalle paikka */}
-                        <iframe
-                            src="https://www.google.com/maps/"
-                            width="400"
-                            height="300"
-                            style={{ border: 0 }}
-                            allowFullScreen=""
-                            loading="lazy"
-                        ></iframe>
-                    </div>
-                </section>
-                <section className="license-info">
-                    <h2>Kalastonhoitomaksu</h2>
-                    <p>Kalastonhoitomaksu on kalastajan pakollinen perusmaksu, joka tulee maksaa, jos on iältään
-                        18–69-vuotias ja kalastaa vieheellä tai pyydyksillä tai ravustaa. Tämä tarkoittaa sitä,
-                        että kun kalastat esimerkiksi virvelillä, verkoilla tai katiskalla, tarvitset aina
-                        kalastonhoitomaksun. Poikkeuksena ovat kalastajat, jotka ehtivät täyttää vähintään
-                        65 vuotta 31.12.2023 mennessä.</p>
-                    <a href="https://www.eraluvat.fi/kalastus/kalastonhoitomaksu" target="_blank"
-                        rel="noopener noreferrer" className="payment-button">
-                        Maksa kalastonhoitomaksu
-                    </a>
-                </section>
-            </main>
-           
-        </div>
-    );
+        <section id="tarkennettu-haku">
+          <h2>Tarkennettu haku</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        </section>
+
+        <section id="yleiset-kysymykset">
+          <h2>Yleiset kysymykset</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        </section>
+
+        <section id="tietoa-meista">
+          <h2>Tietoa meistä</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        </section>
+
+        <section id="kalastonhoito-maksu">
+          <h2>Kalastonhoito maksu</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          <button className="payment-button">Maksa kalastonhoito maksu</button>
+        </section>
+      </main>
+    </div>
+  );
 };
 
-export default Homepage
+export default Homepage;
--- src/components/topbar/SignIn.tsx
+++ src/components/topbar/SignIn.tsx
@@ -5,18 +5,13 @@ import '../styles/TopBar.css'
 const SignIn = () => {
     const textContent = useSelector((state:AppState) => state.selectedLanguage.content)
 
-    return (
-        <div className="login-box">
-            <button onClick={onClose}>Close</button>
-            <p>{data.email}</p>
-            <input type="email" placeholder={data.email} />
-            <p>{data.password}</p>
-            <input type="password" placeholder={data.password} />
-            <p>{data.passwordDescription}</p>
-            <button>{data.loginButton}</button>
-            <button>{data.createAccountButton}</button>
-            <p>{data.createAccountButtonDescription}</p>
+
+    return(
+        <div>
+            <div className="signin">
+                {textContent.loginButton.login}
+            </div>
         </div>
+    )
 }
 export default SignIn
-
