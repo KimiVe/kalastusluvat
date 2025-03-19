513502b4ec9421a2c648cd7b97a28a34e777e110
Kimi <104141050+KimiVe@users.noreply.github.com>
2025-03-19 19:24:29 +0200

    backend vaihto

--- src/services/licenceService.tsx
+++ src/services/licenceService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const getAll = async () => {
-    const response = await axios.get('http://localhost:3000/licences')
+    const response = await axios.get('https://kalastusluvatbackend-production.up.railway.app/users')
     return response.data
 }
 
