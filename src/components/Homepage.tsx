7f6b7edc22565bf9fe8ef04021c4a2998bfe6039
AHir <alihirv@uef.fi>
2025-03-21 00:39:09 +0200

    korjattu homepage käyttämään .json luokkaa

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,52 +3,43 @@ import './styles/TopBar.css'
 import './styles/Links.css'
 import 'react-dropdown/style.css'
 import './topbar/TopBar.tsx';
+import { Link } from "react-router-dom";
 import './Footer.tsx'
 import './topbar/MyInformation.tsx'
-
-const handleButtonClick = () => {
-  window.location.href = "https://www.eraluvat.fi/kalastus/kalastonhoitomaksu";
-};
+import { AppState } from '../store'
+import { useSelector} from "react-redux";
 
 const Homepage = () => {
-  return (
-    <div className="homepage">
-      <main>
-        <section id="etusivu" className="info-section">
-          <div className="content-container">
-            <div className="content-box text-content">
-              <h1 className="content-text-header">Yli 1000 kalalupaa helposti saatavilla</h1>
-              <p className="content-text">
-                Sivustomme tarjoaa helpon ja kätevän tavan löytää ja ostaa yli 1000 
-                kalastuslupaa suoraan osakaskunnilta. Olitpa sitten intohimoinen kalastaja 
-                tai vasta-alkaja, meiltä löydät tarvitsemasi luvat vaivattomasti netin kautta. 
-                Säästä aikaa ja vaivaa – tutustu laajaan valikoimaamme ja hanki kalastuslupasi jo tänään!
-              
-              </p>
-            </div>
-          </div>
-        </section>
+  const textContent = useSelector((state: AppState) => state.selectedLanguage.content);
 
-        <section id="kalastonhoito-maksu" className="info-section">
-          <div className="content-container">
-            <div className="content-box text-content">
-              <h2 className="content-text-header">Kalastonhoito maksu</h2>
-              <p className="content-text">
-                Muista maksaa kalastonhoitomaksu! 
-                Kalastonhoitomaksu on kalastajan pakollinen perusmaksu, joka tulee maksaa, 
-                jos on iältään 18–69-vuotias ja kalastaa vieheellä tai pyydyksillä tai ravustaa. 
-                Tämä tarkoittaa sitä, että kun kalastat esimerkiksi virvelillä, verkoilla tai 
-                katiskalla, tarvitset aina kalastonhoitomaksun. Poikkeuksena ovat kalastajat, 
-                jotka ehtivät täyttää vähintään 65 vuotta 31.12.2023 mennessä.
-              </p>
-              <button className="kalastonhoito-btn" onClick={handleButtonClick}>
-                Maksa kalastonhoito maksu
-              </button>
-            </div>
-          </div>
-        </section>
-      </main>
-    </div>
+  const handleButtonClick = () => {
+      window.location.href = "https://www.eraluvat.fi/kalastus/kalastonhoitomaksu";
+  };
+
+  return (
+      <div className="homepage">
+          <main>
+              <section id="etusivu" className="info-section">
+                  <div className="content-container">
+                      <div className="content-box text-content">
+                          <h1 className="content-text-header">{textContent.frontPage.text0Header}</h1>
+                          <p className="content-text">{textContent.frontPage.text0Content}</p>
+                      </div>
+                  </div>
+              </section>
+              <section id="kalastonhoito-maksu" className="info-section">
+                  <div className="content-container">
+                      <div className="content-box text-content">
+                          <h2 className="content-text-header">{textContent.frontPage.text1Header}</h2>
+                          <p className="content-text">{textContent.frontPage.text1Content}</p>
+                          <button className="kalastonhoito-btn" onClick={handleButtonClick}>
+                              {textContent.frontPage.text1Button}
+                          </button>
+                      </div>
+                  </div>
+              </section>
+          </main>
+      </div>
   );
 };
 
--- src/components/Shareholder.tsx
+++ src/components/Shareholder.tsx
@@ -1,10 +1,26 @@
-{/*osakaskunnansivu*/}
+import React from 'react';
+import './Shareholder.css';
+import './textContentFI.json';
+
+const osakaskunnat = [
+    { nimi: 'Änkilän kalastusosakaskunta', kuvaus: 'Puheenjohtaja Hannu Toivanen on ollut mukana viime vuosituhannelta saakka tekemässä Hiitolanjoen ihmettä.', kuva: 'ankila.jpg' },
+    { nimi: 'Hiitolanjoen kalastusosakaskunta', kuvaus: 'Laatokan järvilohi ei jää enää Kangaskosken patoihin, ja pian järvitaimenet voivat nousta jo Torsanjärvelle saakka.', kuva: 'hiitola.jpg' },
+    
+];
+
+
 const Shareholder = () => {
     return (
-        <div>
-            osakaskunta
+        <div className="shareholder-container">
+            {osakaskunnat.map((osakaskunta, index) => (
+                <div key={index} className="shareholder-box" onClick={() => alert(osakaskunta.nimi)}>
+                    <img src={osakaskunta.kuva} alt={osakaskunta.nimi} className="shareholder-image" />
+                    <h2 className="content-text-header">{osakaskunta.nimi}</h2>
+                    <p className="content-text">{osakaskunta.kuvaus}</p>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Shareholder
+export default Shareholder;
