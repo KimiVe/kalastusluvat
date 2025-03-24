7a77729bc70ed1e80a877ed14ceb7e792c9d689d
Kimi <104141050+KimiVe@users.noreply.github.com>
2025-03-24 18:19:04 +0200

    backendurl

--- src/services/licenceService.tsx
+++ src/services/licenceService.tsx
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const getAll = async () => {
-    //const response = await axios.get('https://kalastusluvatbackend-production.up.railway.app/licences')
-    const response = await axios.get('http://localhost:3000/licences')
+    const response = await axios.get('https://kalastusluvatbackend-production.up.railway.app/licences')
     return response.data
 }
 
